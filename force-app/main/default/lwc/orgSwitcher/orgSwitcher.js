f9c2c327411bd694ae30bad6783fee2922ea5771
aritra mukherjee <aritramukherjee@aritras-MacBook-Air.local>
2025-05-02 00:41:47 +0530

    Changes to include filter and sort dropdown

--- force-app/main/default/lwc/orgSwitcher/orgSwitcher.js
+++ force-app/main/default/lwc/orgSwitcher/orgSwitcher.js
@@ -1,43 +1,73 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getAvailableOrgs from '@salesforce/apex/OrgSwitcherController.getAvailableOrgs';
 
 export default class OrgSwitcher extends LightningElement {
     items = [];
     error;
-    isLoading = true; // Spinner control
+    isLoading; // Spinner control
+    sortBy = 'Last_Login_Time__c'; // Default sort field
+    filterBy = 'All'; // Default filter field
+    
+    filterOptions = [
+        { label: 'All Orgs', value: 'All' },
+        { label: 'Active Orgs', value: 'Active_Orgs' },
+        { label: 'Inactive Orgs', value: 'Inactive_Orgs' },
+        { label: 'SSO Enabled', value: 'SSO_Enabled' },
+        { label: 'SSO Disabled', value: 'SSO_Disabled' },
+    ];
+    sortOptions = [
+        { label: 'Last Login', value: 'Last_Login' },
+        { label: 'Name', value: 'Name' }
+    ];
+
+    handleGetData() {
+        this.isLoading = true; // Show spinner while loading
+        getAvailableOrgs({ filterBy : this.filterBy, sortBy : this.sortBy })
+        .then((data) => {
+            this.items = data.map((record, index) => ({
+                serialNumber: index + 1,
+                id: record.Id,
+                name: record.Name,
+                adminUserName: record.Admin_User_Name__c,
+                adminUserPassword: record.Admin_User_Password__c,
+                loginUrl: record.Login_URL__c,
+                connectedAppId: record.Connected_App_Id__c,
+                ipdInitiatedLoginUrl: record.IdP_Initiated_Login_URL__c,
+                icon: record.Org_Icon__c,
+                isActive: record.IsActive__c,
+                isActiveText: record.IsActive__c ? 'Yes' : 'No',
+                isSSOEnabled: record.SSO_Enabled__c,
+                isSSOEnabledText: record.SSO_Enabled__c ? 'Yes' : 'No',
+                lastLoginTime: record.Last_Login_Time__c,
+                lastLoginStatus: record.Last_Login_Status__c
+            }));
+            this.error = undefined;
+        })
+        .catch((error) => {
+            this.error = error;
+            console.error('Error fetching available orgs:', error);
+        })
+        .finally(() => {
+            this.isLoading = false; // Hide spinner after data is loaded
+        });
+    }
 
     connectedCallback() {
-        this.isLoading = true; // Show spinner while loading
-        getAvailableOrgs()
-            .then((data) => {
-                this.items = data.map((record, index) => ({
-                    serialNumber: index + 1,
-                    id: record.Id,
-                    name: record.Name,
-                    adminUserName: record.Admin_User_Name__c,
-                    adminUserPassword: record.Admin_User_Password__c,
-                    loginUrl: record.Login_URL__c,
-                    connectedAppId: record.Connected_App_Id__c,
-                    ipdInitiatedLoginUrl: record.IdP_Initiated_Login_URL__c,
-                    icon: record.Org_Icon__c,
-                    isActive: record.IsActive__c,
-                    isActiveText: record.IsActive__c ? 'Yes' : 'No',
-                    isSSOEnabled: record.SSO_Enabled__c,
-                    isSSOEnabledText: record.SSO_Enabled__c ? 'Yes' : 'No',
-                    lastLoginTime: record.Last_Login_Time__c,
-                    lastLoginStatus: record.Last_Login_Status__c
-                }));
-                this.error = undefined;
-            })
-            .catch((error) => {
-                this.error = error;
-                console.error('Error fetching available orgs:', error);
-            })
-            .finally(() => {
-                this.isLoading = false; // Hide spinner after data is loaded
-            });
+        this.handleGetData();
     }
-    
+
+    handleFilterChange(e){
+        this.filterBy = e.detail.value;
+        console.log('this.filterBy => ', this.filterBy);
+        this.handleGetData();
+    }
+
+    handleSortChange(e){
+        this.sortBy = e.detail.value;
+        console.log('this.sortBy => ', this.sortBy);
+        this.handleGetData();
+    }
+
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
